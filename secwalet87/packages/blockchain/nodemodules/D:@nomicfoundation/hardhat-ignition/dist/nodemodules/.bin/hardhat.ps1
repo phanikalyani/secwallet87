246fbb5d4884c1e6390ca25ad96eae3f20f942a2
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2025-06-29 16:05:09 +0530

    Add files via upload

---
+++ secwalet87/packages/blockchain/nodemodules/D:@nomicfoundation/hardhat-ignition/dist/nodemodules/.bin/hardhat.ps1
@@ -0,0 +1,41 @@
+#!/usr/bin/env pwsh
+$basedir=Split-Path $MyInvocation.MyCommand.Definition -Parent
+
+$exe=""
+$pathsep=":"
+$env_node_path=$env:NODE_PATH
+$new_node_path="D:\secwallet51\secwallet87\secwallet87\node_modules\.pnpm\hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52\node_modules\hardhat\internal\cli\node_modules;D:\secwallet51\secwallet87\secwallet87\node_modules\.pnpm\hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52\node_modules\hardhat\internal\node_modules;D:\secwallet51\secwallet87\secwallet87\node_modules\.pnpm\hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52\node_modules\hardhat\node_modules;D:\secwallet51\secwallet87\secwallet87\node_modules\.pnpm\hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52\node_modules;D:\secwallet51\secwallet87\secwallet87\node_modules\.pnpm\node_modules"
+if ($PSVersionTable.PSVersion -lt "6.0" -or $IsWindows) {
+  # Fix case when both the Windows and Linux builds of Node
+  # are installed in the same directory
+  $exe=".exe"
+  $pathsep=";"
+} else {
+  $new_node_path="/proc/cygdrive/d/secwallet51/secwallet87/secwallet87/node_modules/.pnpm/hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/internal/cli/node_modules:/proc/cygdrive/d/secwallet51/secwallet87/secwallet87/node_modules/.pnpm/hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/internal/node_modules:/proc/cygdrive/d/secwallet51/secwallet87/secwallet87/node_modules/.pnpm/hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/node_modules:/proc/cygdrive/d/secwallet51/secwallet87/secwallet87/node_modules/.pnpm/hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules:/proc/cygdrive/d/secwallet51/secwallet87/secwallet87/node_modules/.pnpm/node_modules"
+}
+if ([string]::IsNullOrEmpty($env_node_path)) {
+  $env:NODE_PATH=$new_node_path
+} else {
+  $env:NODE_PATH="$new_node_path$pathsep$env_node_path"
+}
+
+$ret=0
+if (Test-Path "$basedir/node$exe") {
+  # Support pipeline input
+  if ($MyInvocation.ExpectingInput) {
+    $input | & "$basedir/node$exe"  "$basedir/../../../../../../hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/internal/cli/bootstrap.js" $args
+  } else {
+    & "$basedir/node$exe"  "$basedir/../../../../../../hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/internal/cli/bootstrap.js" $args
+  }
+  $ret=$LASTEXITCODE
+} else {
+  # Support pipeline input
+  if ($MyInvocation.ExpectingInput) {
+    $input | & "node$exe"  "$basedir/../../../../../../hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/internal/cli/bootstrap.js" $args
+  } else {
+    & "node$exe"  "$basedir/../../../../../../hardhat@2.25.0_bufferutil@4_ddc633825d21bff7c6893923abcdff52/node_modules/hardhat/internal/cli/bootstrap.js" $args
+  }
+  $ret=$LASTEXITCODE
+}
+$env:NODE_PATH=$env_node_path
+exit $ret
